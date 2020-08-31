abdcece1f5320a9ed94f6d0fd9f74a6e8bad56da
adavidkhowantolim <44863712+adavidkhowantolim@users.noreply.github.com>
2020-08-31 09:35:59 +0700

    soal 2

--- Tugas-6/tugas6.js
+++ Tugas-6/tugas6.js
@@ -10,3 +10,17 @@ let luasLingkaran = LuasLingkaran(r);
 
 console.log(luasLingkaran);
 console.log(KelilingLingkaran(r));
+
+// SOAL 1
+console.log("------ SOAL 2 ------")
+
+let kalimat = ""
+let saya = "saya";
+let adalah = "adalah";
+let seorang = "seorang";
+let frontend = "frontend";
+let developer = "developer";
+
+let addKalimat = (a, b, c, d, e) => `${a} ${b} ${c} ${d} ${e}`
+
+console.log(addKalimat(saya, adalah, seorang, frontend, developer));
