a279f6392964c41085af378d1cb3e0dedb368cbc
adavidkhowantolim <44863712+adavidkhowantolim@users.noreply.github.com>
2020-09-01 19:12:18 +0700

    Release 2

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -1,5 +1,5 @@
 // SOAL 1
-console.log("------ Soal 1 ------")
+console.log("------ Soal 1 ------");
 
 class Animal {
   
@@ -61,3 +61,42 @@ sungokong.yell() // "Auooo"
  
 var kodok = new Frog("buduk")
 kodok.jump() // "hop hop" 
+
+// SOAL 2
+console.log("------ Soal 2 ------");
+
+class Clock {
+  constructor({template}){
+    this._template = template;
+    this._timer = 0;
+  }
+
+  render = () => {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+    
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+    
+    let output = this._template.replace('h', hours).replace('m', mins).replace('s', secs);
+    
+    console.log(output);
+  } 
+
+  stop(){
+    clearInterval(this._timer);
+  }
+
+  start(){
+    this.render();
+    this._timer = setInterval(this.render, 1000);
+  }
+}
+
+var clock = new Clock({template: 'h:m:s'});
+clock.start();  
