0b57270503404d5565f594d601dad3ab6da94b2f
adavidkhowantolim <44863712+adavidkhowantolim@users.noreply.github.com>
2020-09-02 10:17:26 +0700

    release soal 1

---
+++ Tugas-8/index.js
@@ -0,0 +1,21 @@
+
+// di index.js
+var readBooks = require('./callback.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000},
+    {name: 'komik', timeSpent: 1000}
+]
+ 
+// Tulis code untuk memanggil function readBooks di sini
+let i = 0;
+readBooks(10000, books[0], function(time){
+  readBooks(time, books[1], function(time){
+    readBooks(time, books[2], function(time){
+      readBooks(time, books[3], function(time){
+      })
+    })
+  })
+});
